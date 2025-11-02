85369ec30a2203eef481f8221ee66f281dafb7b3
Keerthi <keerthipadamati16@gmail.com>
2025-11-02 20:10:49 +0530

    Update AudioConverter.java

--- SmartFileConverter/src/smartfileconverter/AudioConverter.java
+++ SmartFileConverter/src/smartfileconverter/AudioConverter.java
@@ -8,5 +8,6 @@ public class AudioConverter
     public File mockConvert(File input, String targetExt) throws IOException 
     {
         File out = new File(input.getParent(),input.getName().replace(".", "_converted."));
-
+         try (FileInputStream fis = new FileInputStream(input);FileOutputStream fos = new FileOutputStream(out))
+         {
 
