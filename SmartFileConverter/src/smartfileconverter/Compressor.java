91b2a0121508595c4ae91126267676b2cefc3ade
Keerthi <keerthipadamati16@gmail.com>
2025-11-04 18:09:31 +0530

    Update THE CODE
    ADDED TRY WITH RESOURCES FILEINOUT ,FILEOUPUT GIPOUTPUT

--- SmartFileConverter/src/smartfileconverter/Compressor.java
+++ SmartFileConverter/src/smartfileconverter/Compressor.java
@@ -8,3 +8,9 @@ public class Compressor
   public File compressText(File input) throws IOException
   {
     File out = new File(input.getParent(),input.getName().replace(".", "_compressed."));
+    try (
+            FileInputStream fis = new FileInputStream(input);
+            FileOutputStream fos = new FileOutputStream(out);
+            GZIPOutputStream gzip = new GZIPOutputStream(fos)
+        ) 
+
